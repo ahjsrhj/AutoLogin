8a234fc2708957b48ddf1e14f6c0f3bae75a963c
rhj <aha199277@qq.com>
2015-05-22 22:21:17 +0800

    add menu

--- app/src/main/java/tk/imrhj/autologin/MainActivity.java
+++ app/src/main/java/tk/imrhj/autologin/MainActivity.java
@@ -1,5 +1,6 @@
 package tk.imrhj.autologin;
 
+import android.app.ActionBar;
 import android.app.Activity;
 
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,7 +18,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     private Button saveInfo;
     private Button login;
@@ -30,6 +32,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
         ChangeColor();
         setContentView(R.layout.activity_main);
         startService(new Intent(this, WifiChangeService.class));
+
+
+
         login = (Button) findViewById(R.id.btn_login);
         saveInfo = (Button) findViewById(R.id.btn_start);
         userName = (EditText) findViewById(R.id.username);
@@ -37,6 +42,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
         LoadData();
         login.setOnClickListener(this);
         saveInfo.setOnClickListener(this);
+
+
 
 
     }
@@ -99,26 +106,35 @@ public class MainActivity extends Activity implements View.OnClickListener {
         }
     }
 
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_activity2, menu);
+
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_settings:
+                break;
+            case R.id.action_help:
+                break;
+            case R.id.action_about:
+                break;
+            case R.id.action_updata:
+                break;
+            case R.id.action_exit:
+                break;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
 
 }
